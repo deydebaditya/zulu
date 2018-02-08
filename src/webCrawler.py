7e9e278adf1a8bce086cb868cdf66f5109715337
Sarosh Farhan <attitude.love.1995@gmail.com>
2018-02-09 02:10:49 +0530

    added auto matic https:// string, user will now only add site's name when prompted

--- src/webCrawler.py
+++ src/webCrawler.py
@@ -1,31 +1,38 @@
 import requests as request
 from bs4 import BeautifulSoup as bs
 
-#queue for storing the visited links
+
+# queue for storing the visited links
 class Queue:
     def __init__(self):
         self.items = []
+
     def isEmpty(self):
         return self.items == []
-    def enqueue(self,item):
-        self.items.insert(0,item)
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
     def dequeue(self):
         return self.items.pop()
+
     def size(self):
         return len(self.items)
+
 
 def zulu(start_url):
     q = Queue()
     q.enqueue(start_url)
-    while(q.isEmpty() == False):
+    while q.isEmpty() == False:
         url = q.dequeue()
         source_code = request.get(url)
         plain_text = source_code.text
         formattedSource = bs(plain_text)
         for link in formattedSource.findAll('a'):
-            href = start_url + "/"+ link.get('href')
+            href = start_url + "/" + link.get('href')
             q.enqueue(href)
             print(href)
+
 
 def __main__():
     print('Welcome to ZULU!')
@@ -37,6 +44,7 @@ def __main__():
     print('Till then, then you can fiddle around and enjoy with me')
     print('###################################\n\n')
     start_url = str(input('Enter the full URL from which I should start crawling:'))
-    zulu(start_url)
+    zulu("https://" + start_url)
+
 
 __main__()
