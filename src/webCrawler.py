3c8c8516ad0e05c5fdab022ff32888e4d00b0185
Debaditya Dey <dey.debaditya@gmail.com>
2018-02-09 10:55:49 +0530

    Major Bugfixes
    1. Fixed problem of URL cascading
    2. Added checks to find if links already contain domain names, if it does, cascade won't be performed.
    3. Added sets for nt performing crawling on links which are already visited.

--- src/webCrawler.py
+++ src/webCrawler.py
@@ -1,5 +1,6 @@
 import requests as request
 from bs4 import BeautifulSoup as bs
+import re
 
 #queue for storing the visited links
 class Queue:
@@ -17,15 +18,32 @@ class Queue:
 def zulu(start_url):
     q = Queue()
     q.enqueue(start_url)
+    visited = set()
+    listed = set()
     while(q.isEmpty() == False):
         url = q.dequeue()
+        print("Popped: "+url)
+        visited.add(url)
         source_code = request.get(url)
         plain_text = source_code.text
-        formattedSource = bs(plain_text)
+        formattedSource = bs(plain_text, "html.parser")
         for link in formattedSource.findAll('a'):
-            href = start_url + "/"+ link.get('href')
-            q.enqueue(href)
-            print(href)
+            if(link.get('href') is "#" or link.get('href') is "" or link.get('href') is " " or visited.__contains__(link.get('href')) or listed.__contains__(link.get('href'))):
+                continue
+            listed.add(link.get('href'))
+            temp = link.get('href')
+            #print(temp)
+            if("mailto" in temp):
+                continue
+            if(("http" not in temp)):
+                if("https" not in temp):
+                    print('Not found URL')
+                    q.enqueue(start_url + "/" + link.get('href'))
+                    print("Crawled URL: " + start_url + "/" + link.get('href'))
+            else:
+                q.enqueue(link.get('href'))
+                print("Crawled URL: " +link.get('href'))
+
 
 def __main__():
     print('Welcome to ZULU!')
